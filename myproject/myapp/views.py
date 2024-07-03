a271c561c2528bce7ff3d2284bcf2705f8bab5b8
ismaily1 <zzuurr123@icloud.com>
2024-07-03 10:20:52 +0300

    permissions

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+
+
+from myproject.myapp.permissions import IsOwner
 from .models import Item
 from .serializers import ItemSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework import generics, pagination
+from .permissions import IsOwner
 
 class ItemListCreate(generics.ListCreateAPIView):
     queryset = Item.objects.all()
@@ -13,4 +17,12 @@ class ItemListCreate(generics.ListCreateAPIView):
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
+class ItemListAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsOwner]
+    pagination_class = pagination.PageNumberPagination
+
+
